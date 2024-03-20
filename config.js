d3b6168d15216d2c41eb6d26398e25ae78aa0a97
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-03-20 17:12:40 +0900

    add config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  nhttp_urls: [
+    "http://localhost:20001",
+    // "https://example2.com/anotherEndpoint",
+    // ....
+  ],
+  port: 4000,
+  clusters: 1,
+
+  server_meta: {
+    "contact": "unset",
+    "pubkey": "0000000000000000000000000000000000000000000000000000000000000000",
+    "description": "nhttp adapter",
+    "name": "nhttp",
+    "software": "git+https://github.com/Yonle/nhttp-adapter",
+    "supported_nips": [1,2,9,11,12,15,16,20,22,33,40,50],
+    "version": require("./package.json").version
+  }
+}
